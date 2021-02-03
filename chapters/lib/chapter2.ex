b3b0092a4e1b24944613d710d974a755ed5469cc
Lord Bagger <erik.bagger.ext@ausy-technologies.de>
2021-02-04 00:50:34 +0100

    renamed Chapter1 to Chapter2

--- chapters/lib/chapter2.ex
+++ chapters/lib/chapter2.ex
@@ -1,11 +1,11 @@
-defmodule Chapter1 do
+defmodule Chapter2 do
   def sum(numbers) when is_list(numbers), do: List.foldl(numbers, 0, fn numbers, acc -> numbers + acc end)
 
   #flattens a list, reverses the order of the elements and returns the squares
-  def with_pipe(list) when is_list(list), do: list |> List.flatten |> Enum.reverse |> Enum.map(fn elem -> elem * elem end)
+  def transform_with_pipe(list) when is_list(list), do: list |> List.flatten |> Enum.reverse |> Enum.map(fn elem -> elem * elem end)
 
   #same thing as above, but without the 'pipe' operator
-  def without_pipe(list) when is_list(list) do
+  def transform_without_pipe(list) when is_list(list) do
     Enum.map(Enum.reverse(List.flatten(list)), fn elem -> elem * elem end)
   end
 end
--- chapters/test/chapter2_test.exs
+++ chapters/test/chapter2_test.exs
@@ -1,19 +1,19 @@
-defmodule Chapter1Test do
+defmodule Chapter2Test do
   use ExUnit.Case
 
   test "sums all numbers from a list" do
-    assert Chapter1.sum([1, 2, 3]) == 6
+    assert Chapter2.sum([1, 2, 3]) == 6
   end
 
   test "flattens a list, reverses and returns the squares for each element (with pipe operator)" do
-    assert Chapter1.with_pipe([1, [2, [3]]]) == [9, 4, 1]
+    assert Chapter2.transform_with_pipe([1, [2, [3]]]) == [9, 4, 1]
   end
 
   test "flattens a list, reverses and returns the squares for each element (without pipe operator)" do
-    assert Chapter1.without_pipe([1, [2, [3]]]) == [9, 4, 1]
+    assert Chapter2.transform_without_pipe([1, [2, [3]]]) == [9, 4, 1]
   end
 
   test "with_pipe and without_pipe functions return the same results" do
-    assert Chapter1.without_pipe([1, [2, [3]]]) == Chapter1.with_pipe([1, 2, 3])
+    assert Chapter2.transform_without_pipe([1, [2, [3]]]) == Chapter2.transform_with_pipe([1, 2, 3])
   end
 end
