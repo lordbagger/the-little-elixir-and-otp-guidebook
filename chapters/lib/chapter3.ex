66f47ac93fab67fbc74fd53955da64421bc65590
Lord Bagger <erik.bagger.ext@ausy-technologies.de>
2021-02-04 17:56:15 +0100

    ping_pong function interacts 10 times before exiting

--- chapters/lib/chapter3.ex
+++ chapters/lib/chapter3.ex
@@ -2,7 +2,7 @@ defmodule Chapter3 do
 
   def ping_pong do
     ping_pid = spawn(Chapter3, :ping, [])
-    pong_pid = spawn(Chapter3, :pong, [])
+    pong_pid = spawn(Chapter3, :pong, [0, 3])
     send(pong_pid, {:pong, ping_pid})
   end
 
@@ -11,16 +11,27 @@ defmodule Chapter3 do
       {:ping, sender_pid} ->
         IO.puts "Pong"
         send(sender_pid, {:pong, self()})
+        ping()
+      {:exit, sender_pid} ->
+        IO.puts("PING: Process terminated")
+        send(sender_pid, :exit)
       _ ->
         IO.puts("Can't handle this message")
     end
   end
 
-  def pong() do
+  def pong(interactions, expected) do
     receive do
       {:pong, sender_pid} ->
+        if interactions == expected do
+          send(sender_pid, {:exit, self()})
+        end
         IO.puts "Ping"
+        new_interactions = interactions + 1
         send(sender_pid, {:ping, self()})
+        pong(new_interactions, expected)
+      :exit ->
+        IO.puts("PONG: Process terminated")
       _ ->
         IO.puts("Can't handle this message")
     end
