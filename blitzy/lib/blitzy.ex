bcaf3d1a4b67041d52f72f1f64db9b07ccb430d5
Lord Bagger <erik.bagger.ext@ausy-technologies.de>
2021-05-23 18:24:56 +0200

    blitzy: spawns workers to perform requests and prints statistics from the results

--- blitzy/lib/blitzy.ex
+++ blitzy/lib/blitzy.ex
@@ -1,18 +1,52 @@
 defmodule Blitzy do
-  @moduledoc """
-  Documentation for `Blitzy`.
-  """
+  def run(n_workers, url) do
+    func = fn -> Blitzy.Worker.start(url) end
 
-  @doc """
-  Hello world.
+    1..n_workers
+    |> Enum.map(fn _ -> Task.async(func) end)
+    |> Enum.map(&Task.await(&1))
+    |> parse_results
+  end
 
-  ## Examples
+  defp parse_results(results) do
+    {successes, _failures} =
+      results
+      |> Enum.split_with(fn r ->
+        {
+          case r do
+            {:ok, _} -> true
+            _ -> false
+          end
+        }
+      end)
 
-      iex> Blitzy.hello()
-      :world
+    total_workers = Enum.count(results)
+    total_success = Enum.count(successes)
+    total_failure = total_workers - total_success
 
-  """
-  def hello do
-    :world
+    data = successes |> Enum.map(fn {:ok, time} -> time end)
+    shortest_time = Enum.min(data)
+    longest_time = Enum.max(data)
+
+    average_time = calculate_average(data)
+
+    IO.puts("""
+    Total workers    : #{total_workers}
+    Successful reqs  : #{total_success}
+    Failed res       : #{total_failure}
+    Average (msecs)  : #{average_time}
+    Longest (msecs)  : #{longest_time}
+    Shortest (msecs) : #{shortest_time}
+    """)
+  end
+
+  defp calculate_average(data) do
+    sum = Enum.sum(data)
+
+    if sum > 0 do
+      sum / Enum.count(data)
+    else
+      0
+    end
   end
 end
--- blitzy/test/blitzy_test.exs
+++ blitzy/test/blitzy_test.exs
@@ -1,8 +1,4 @@
 defmodule BlitzyTest do
   use ExUnit.Case
   doctest Blitzy
-
-  test "greets the world" do
-    assert Blitzy.hello() == :world
-  end
 end
